b92722e09e3d59750e1c3d4f778e988cd9e7baed
mendsxyz <princemendie03@gmail.com@gmail.com>
2025-02-10 00:49:28 +0100

    update main.js

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// Main
+const main = document.querySelector("main");
+
+// Nav toggle
+const bottomBar = document.querySelector("nav.bottombar.xs-sm-md");
+const bb_toggle = document.querySelector(".bottombar-toggle");
+bb_toggle.addEventListener("click", () => {
+  bottomBar.classList.toggle("active");
+  main.addEventListener("click", menuOpenOk_touchBg);
+});
+function menuOpenOk_touchBg(e) {
+  if (main.contains(e.target)) {
+    bottomBar.classList.remove("active");
+  }
+}
+
+// Google sign in function
+let googleUser = {};
+
+let startApp = function() {
+  gapi.load("auth2", function() {
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
+      client_id: "754387532769-2vrnrga8vunfjk7hbislp168u36hjsr2.apps.googleusercontent.com",
+      cookiepolicy: "single_host_origin",
+      scope: "profile email https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/drive.readonly"
+    });
+    attachSignin(document.querySelector("#signInBtn"));
+  });
+};
+
+function onSignIn(googleUser) {
+  let profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+
+function attachSignin(element) {
+  console.log(element.id);
+  auth2.attachClickHandler(element, {},
+  function(googleUser) {
+    document.querySelector(".name").innerText = "Signed in: " +
+    googleUser.getBasicProfile().getName();
+  },
+  function(error) {
+    alert(JSON.stringify(error, undefined, 2));
+  });
+}
+
+startApp();
