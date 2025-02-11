749f390e66d267a3cc481963cb8448d22b8b2f40
mendsxyz <princemendie03@gmail.com@gmail.com>
2025-02-11 02:38:07 +0100

    update

--- js/main.js
+++ js/main.js
@@ -62,9 +62,22 @@ function attachSignin(element) {
   function(googleUser) {
     document.querySelector(".name").innerText = "Signed in: " +
     googleUser.getBasicProfile().getName();
+    
+    // Sign in successful
+    
   },
   function(error) {
     alert(JSON.stringify(error, undefined, 2));
+  });
+}
+
+const signOutBtn = document.querySelector("#signOutBtn");
+signOutBtn.addEventListener("click", signOut);
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
   });
 }
 
