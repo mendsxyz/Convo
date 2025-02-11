10dbdf34b97d83ef659736f3d8adc047173a0ad4
mendsxyz <princemendie03@gmail.com@gmail.com>
2025-02-11 14:16:05 +0100

    update

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ const signOutBtn = document.querySelector("#signOutBtn");
 signOutBtn.addEventListener("click", signOut);
 
 function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
+  let auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
     document.querySelector(".name").innerText = "Signed out";
