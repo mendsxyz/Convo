ba041c99ab069d8611a5f6d590862cbe633409d5
mendsxyz <princemendie03@gmail.com>
2025-02-15 07:51:33 +0100

    update

--- js/posts.js
+++ js/posts.js
@@ -1,5 +1,7 @@
 // Posts Object Array
 
+const contentView = document.querySelector(".content-view");
+
 const posts = {
   
   //--2
@@ -8,4 +10,17 @@ const posts = {
     "body": "Post 1 body content"
   }
   //--1
+}
+
+for (const post of posts) {
+  function postBlob() {
+    return `
+      <div>
+        <span class="post-title">${post[index].title}</span>
+        <span class="post-body">${post[index].body}</span>
+      </div>
+    `
+  }
+  
+  contentView.innerHTML = postBlob();
 }
