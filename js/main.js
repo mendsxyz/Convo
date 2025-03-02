0246bbdb16787efec18b48958989f5b9941f3493
mendsxyz <princemendie03@gmail.com>
2025-03-02 07:08:11 +0100

    update main.js

--- js/main.js
+++ js/main.js
@@ -218,6 +218,10 @@ document.addEventListener("DOMContentLoaded", () => {
               UI.animation_wrapper.style.display = "flex";
             } else {
               alert("Email verified successfully!");
+
+              UI.hero.classList.remove("await-auth");
+              UI.loader.insertAdjacentHTML("afterbegin", welcomeScr);
+              UI.animation_wrapper.style.display = "none";
             }
           }
 
@@ -1546,7 +1550,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
 
       // Signed out
-      
+
       UI.loader.classList.remove("active");
       UI.hero.classList.remove("await-auth");
       UI.auth_content.classList.remove("active");
